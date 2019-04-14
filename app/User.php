2180ca3f3fa5e08931153444f0ab8f0466301232
hamdane <youssefamir89@gmail.com>
2019-04-14 23:16:23 +0100

    Update laravel User model

--- app/User.php
+++ app/User.php
@@ -16,7 +16,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'email',
+	    'password'
     ];
 
     /**
@@ -36,4 +37,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function generateToken()
+    {
+        //$this->api_token = str_random(60);
+        $this->api_token = encrypt($this->id.'_'.$this->timestamps);
+        $this->save();
+        return $this->api_token;
+    }
 }
