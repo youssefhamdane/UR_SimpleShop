96f4114ce4eba1874c80723594b1c4460714c16d
hamdane <youssefamir89@gmail.com>
2019-04-14 21:10:52 +0100

    Add laravel shops table migration

---
+++ database/migrations/2019_04_07_163411_create_shops_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('picture');
+            $table->string('name');
+            $table->string('email');
+            $table->string('city');
+            $table->decimal('coordinate_longitude',11, 8);
+            $table->decimal('coordinate_latitude',10, 8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
