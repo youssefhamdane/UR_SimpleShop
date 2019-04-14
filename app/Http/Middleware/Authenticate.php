21b1cc0198915f04e90dae4aaa10344176bf69d7
hamdane <youssefamir89@gmail.com>
2019-04-15 00:58:34 +0100

    Update laravel Authenticate middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -8,14 +8,13 @@ class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
+     * Updated to return json response
      *
      * @param  \Illuminate\Http\Request  $request
      * @return string
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+        return response()->json(['error' => 'Unauthenticated'],201);
     }
 }
