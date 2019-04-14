a269b0181ba5f8e71559ac42e5e457a674acd422
hamdane <youssefamir89@gmail.com>
2019-04-15 00:57:13 +0100

    Update laravel loginController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,62 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+	/**
+	 * Handle a login request to the application.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+	 *
+	 * @throws \Illuminate\Validation\ValidationException
+	 */
+	public function login(Request $request)
+	{
+		$this->validateLogin($request);
+
+		// If the class is using the ThrottlesLogins trait, we can automatically throttle
+		// the login attempts for this application. We'll key this by the username and
+		// the IP address of the client making these requests into this application.
+
+		if ($this->attemptLogin($request)) {
+			$user = $this->guard()->user();
+			$user->generateToken();
+			return response()->json([
+				'data' => $user->toArray(),
+			]);
+		}
+
+		return $this->sendFailedLoginResponse($request);
+	}
+
+	/**
+	 * Logout user
+	 *
+	 * @param Request $request
+	 *
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+    public function logout(Request $request)
+    {
+        $user = Auth::guard('api')->user();
+
+        if ($user) {
+            $user->api_token = null;
+            $user->save();
+        }
+
+        return response()->json(['data' => 'User logged out.'], 200);
+    }
+
+	/** Check user is logged
+	 *
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+    public function islogged(){
+        $user = Auth::guard('api')->user();
+        if ($user)
+        return response()->json(['data' => 'User logged.'], 200);
+        else  return response()->json(['error' => 'Unauthenticated'],201);
     }
 }
