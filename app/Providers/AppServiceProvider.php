d8ce53243d6aa4c5e3c99b3fa4dbd88183479c2b
hamdane <youssefamir89@gmail.com>
2019-04-15 23:51:37 +0100

    Update laravel AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; // Import Schema
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // mysql String length
+	    Schema::defaultStringLength(255);
     }
 }
