dac0a8e948e83f988c29dce39e7060792e056675
hamdane <youssefamir89@gmail.com>
2019-04-15 01:01:24 +0100

    Update laravel api route

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,17 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 });
+Route::group(['middleware' => 'auth:api'], function() {
+	Route::get('shops', 'ShopController@index');
+	Route::get('shops/liked', 'ShopController@liked');
+	Route::get('shops/like/{shop}', 'ShopController@like');
+	Route::get('shops/dislike/{shop}', 'ShopController@dislike');
+	Route::get('shops/remove/{shop}', 'ShopController@remove');
+});
+
+Route::get('islogged', 'Auth\LoginController@islogged');
+Route::post('register', 'Auth\RegisterController@register');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout');
