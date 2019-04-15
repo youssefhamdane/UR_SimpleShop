dbf6917c11be6c95e70bb2521f6bedadbf8edfcc
hamdane <youssefamir89@gmail.com>
2019-04-15 22:38:02 +0100

    Add angular globals variables

---
+++ angular/src/app/globals.ts
@@ -0,0 +1,13 @@
+'use strict';
+export const GlobalVariable = Object({
+    /**
+     * API URL
+     * @param string
+     */
+    BASE_API_URL: '',
+    /**
+     *  USER TOKEN
+     * @param string
+     */
+    API_TOKEN:''
+});
